9de655f30fcb874e86b2a6e6c887b7b13c4a4077
“Tatyana <umnova.panther@yandex.ru>
2024-06-05 13:25:26 +0500

    adding files implementing the iterator

---
+++ Randoms.java
@@ -0,0 +1,30 @@
+package ru.netology;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    int min, max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return new Random().nextInt((max + 1) - min) + min;
+            }
+        };
+    }
+
+}
